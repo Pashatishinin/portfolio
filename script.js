7f1ff83577a0d69b81187d0f30cafe8f0f83fc97
PaTish <pashatishinin@gmail.com>
2024-07-02 22:08:57 +0200

    about section

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let menuIcon = document.querySelector(".menu-icon");
 menuIcon.addEventListener("click",()=>{
     menuIcon.classList.toggle("active");
 });
+
+// switch between about buttons
+const buttons = document.querySelectorAll(".about-btn button");
+const contents = document.querySelectorAll(".content");
+
+buttons.forEach((button, index) => {
+    button.addEventListener("click",() => {
+        contents.forEach(content => content.style.display = "none");
+        contents[index].style.display = "block";
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+    });
+});
