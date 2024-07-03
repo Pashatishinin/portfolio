3251826be70aa113d5a94c8e5280a724821f2981
PaTish <pashatishinin@gmail.com>
2024-07-03 22:30:08 +0200

    hobby

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ var mixer = mixitup('.portfolio-gallery', {
         duration: 500
     }
 });
+
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        576:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        1200: {
+            slidesPerView:3,
+            spaceBetween:20,
+        },
+    }
+});
