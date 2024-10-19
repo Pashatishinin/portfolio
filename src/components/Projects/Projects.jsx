8e905f82f8df844295736ce36f06bd197b305e68
PaTish <pashatishinin@gmail.com>
2024-10-19 21:05:56 +0200

    commit

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,65 @@
+import img2 from '../../../public/projects/olivermckenzie.png'
+import img4 from '../../../public/projects/what_to_watch.png'
+import img5 from '../../../public/projects/vote_note.png'
+import img6 from '../../../public/projects/inuka.png'
+import img7 from '../../../public/projects/bentezhne.png'
+import img8 from '../../../public/projects/meetme.png'
+
+const projects = [
+  {
+    title: "Portfolio",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: '',
+  },
+  {
+    title: "Oliver McKenzie | Designer",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img2,
+  },
+  {
+    title: "Ahin Anburaja | Product Manager",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: "",
+  },
+  {
+    title: "The What To Watch",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img4,
+  },
+  {
+    title: "Vote & Note",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img5,
+  },
+  {
+    title: "INUKA",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img6,
+  },
+  {
+    title: "Ben Tezhne",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img7,
+  },
+  {
+    title: "Meet Me",
+    link: "pavlotishynin.com",
+    github: "",
+    skills: "",
+    img: img8,
+  },
+];
